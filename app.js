554de5a36bbe10640c8ab12bb0501ea750a3bf1e
Lucas Martins <lucas.martins@clinicasim.com>
2022-07-04 17:16:17 -0300

    API inicial

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 
+app.use("/products",(req, res, next) => {
+    const data = [
+        {"img":"https://i.imgur.com/nOFet9u.jpg",
+        "name":"XRD Active Shoes",
+        "price":"$1,999.99",
+        "quantity":10}
+        ]
+    
+    res.send((data));
+});
+
 app.use((req, res, next) => {
     res.status(200).send({
         mensagem: 'API conectada'
     });
 });
+
+
 
 module.exports = app;
